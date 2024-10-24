a2fd215315df98fa372c9f63598440a510c361b2
bobbbbbbbbbby <xiongzijun20a@mails.ucas.ac.cn>
2024-10-24 14:02:46 +0800

    problem 26 solved

---
+++ 26/Solution.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Globalization;
+
+namespace leetcode._26;
+
+public class Solution
+{
+    public int RemoveDuplicates(int[] nums)
+    {
+        int k = 1;
+        int curr = nums[0];
+        for(int i = 1; i < nums.Length; i++) {
+            if(nums[i] != curr) {
+                nums[k] = nums[i];
+                curr = nums[k];
+                k++;
+            }
+        }
+        return k;
+    }
+}
